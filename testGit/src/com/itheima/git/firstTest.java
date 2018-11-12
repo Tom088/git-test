9b6133781371d9dc5549eec5104917a436608618
wwh <1064700445@qq.com>
2018-11-12 21:35:18 +0800

    third

--- testGit/src/com/itheima/git/firstTest.java
+++ testGit/src/com/itheima/git/firstTest.java
@@ -4,5 +4,7 @@ public class firstTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello 666666");
+        System.out.println("hello 666666");
+        System.out.println("hello 666666");
     }
 }
