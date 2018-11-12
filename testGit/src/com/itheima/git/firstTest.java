6e2b5f864267bb8ffe2f312944f0b2cdcf86e5e7
wwh <1064700445@qq.com>
2018-11-12 21:27:55 +0800

    second

--- testGit/src/com/itheima/git/firstTest.java
+++ testGit/src/com/itheima/git/firstTest.java
@@ -3,5 +3,6 @@ package com.itheima.git;
 public class firstTest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello 666666");
     }
 }
